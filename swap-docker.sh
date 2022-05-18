a9b439e3b8bb0d4f537c1c592fbc52deb4c998a3
anvuong <anvb@vng.com.vn>
2022-05-17 21:18:15 -0700

    add build

--- swap-docker.sh
+++ swap-docker.sh
@@ -2,4 +2,4 @@ docker stop audio-api
 docker rm audio-api
 docker images | grep audio-api | awk '{print $3}' | xargs -L1 docker rmi
 docker load -i audio-api.$1
-docker run -d -v /root/voice-data:/data -p 8580:8580 audio-api:$1
+docker run --name audio-api -d -v /root/voice-data:/data -p 8580:8580 audio-api:$1
